b913174b3c892cb514b855cca25193d850fc692f
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-09-16 16:44:25 +0800

    basic intro slider screen

---
+++ app/src/main/java/com/ridhwandaud/introscreen/PrefManager.java
@@ -0,0 +1,38 @@
+package com.ridhwandaud.introscreen;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by ridhwan on 16/09/2017.
+ */
+
+
+
+public class PrefManager {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    int PRIVATE_MODE = 0;
+
+    private static final String PREF_NAME = "intro-welcome";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "isFirstTimeLaunch";
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+}
